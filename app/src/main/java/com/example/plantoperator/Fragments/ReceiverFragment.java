d4f35131be18a938aa432e11b89c5c3b8cf4aed5
darshrm <darshan.marathe13@gmail.com>
2020-11-09 15:34:18 +0530

    calculate distance and duration

--- app/src/main/java/com/example/plantoperator/Fragments/ReceiverFragment.java
+++ app/src/main/java/com/example/plantoperator/Fragments/ReceiverFragment.java
@@ -5,10 +5,12 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -42,7 +44,20 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.GeoPoint;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.DistanceMatrixApi;
+import com.google.maps.GeoApiContext;
+import com.google.maps.PendingResult;
+import com.google.maps.model.DirectionsResult;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -50,9 +65,16 @@ import java.util.Map;
 import javax.annotation.Nullable;
 
 public class ReceiverFragment extends Fragment implements OnMapReadyCallback {
-
+    List<Double> source_latitudes = new ArrayList<>();
+    List<Double> source_longitudes = new ArrayList<>();
+    List<Double> destination_latitudes = new ArrayList<>();
+    List<Double> destination_longitudes = new ArrayList<>();
+    List<String> distance_array = new ArrayList<>();
+    List<String> duration_array = new ArrayList<>();
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private GeoApiContext mGeoApiContext = new GeoApiContext.Builder()
+            .apiKey("AIzaSyB_A_3lfpzS0oSp0p9Q2XTqOtBe9TmbDuo").build();
     SupportMapFragment mapFragment;
     List<Marker> marks = new ArrayList<Marker>();
     List<String> data = new ArrayList<String>();
@@ -103,7 +125,6 @@ public class ReceiverFragment extends Fragment implements OnMapReadyCallback {
         mBottomSheetBehavior = BottomSheetBehavior.from(mCustomBottomSheet);
         mHeaderLayout = view.findViewById(R.id.header_layout);
         mHeaderImage = view.findViewById(R.id.header_arrow);
-        ;
         programmingList = (RecyclerView) view.findViewById(R.id.programmingList);
         programmingList.setLayoutManager(new LinearLayoutManager(getActivity()));
 
@@ -134,6 +155,7 @@ public class ReceiverFragment extends Fragment implements OnMapReadyCallback {
                 mHeaderImage.setRotation(slideOffset * 180);
             }
         });
+
 
 
         // Inflate the layout for this fragment
@@ -217,17 +239,46 @@ public class ReceiverFragment extends Fragment implements OnMapReadyCallback {
                                     lat = g.getLatitude();
                                     lng = g.getLongitude();
 
+                                    //store in source array
+                                    source_latitudes.add(lat);
+                                    source_longitudes.add(lng);
 
                                     //vehicle number
                                     Map user = (Map) user_doc.get("userDetails");
                                     String name = String.valueOf(user.get("name"));
 
+                                    //Customer Location
+                                    Map customer = (Map) user_doc.get("customerDetails");
+                                    GeoPoint destination = (GeoPoint) customer.get("location");
+
+                                    //store in destination array
+                                    destination_latitudes.add(destination.getLatitude());
+                                    destination_longitudes.add(destination.getLongitude());
+
+
                                     data.add(name);
                                     //Mark Location
                                     LatLng location = new LatLng(lat, lng);
-                                    Marker m = mMap.addMarker(new MarkerOptions().position(location).title(name).icon(bitmapDescriptorFromVector(getContext(), R.drawable.ic_baseline_local_shipping_24)));
+                                    Marker m = mMap.addMarker(new MarkerOptions().position(location).title("Name:"+name).icon(bitmapDescriptorFromVector(getContext(), R.drawable.ic_baseline_local_shipping_24)));
                                     mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
                                     marks.add(m);
+
+                                    for(Double source_lat:source_latitudes){
+                                        Log.d("source_lat",""+source_lat);
+                                    }
+
+                                    for(Double source_lng:source_longitudes){
+                                        Log.d("source_lng",""+source_lng);
+                                    }
+
+                                    for(Double destination_lat:destination_latitudes){
+                                        Log.d("destination_lat",""+destination_lat);
+                                    }
+
+                                    for(Double destination_lng:destination_longitudes){
+                                        Log.d("destination_lng",""+destination_lng);
+                                    }
+
                                 }
                             }
                             initializeRecyclerView(programmingList, mBottomSheetBehavior);
@@ -268,8 +319,39 @@ public class ReceiverFragment extends Fragment implements OnMapReadyCallback {
                                 mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
                                     @Override
                                     public boolean onMarkerClick(Marker marker) {
-                                       showMessage(marker);
-                                       return true;
+                                        enableStrictMode();
+                                        OkHttpClient client = new OkHttpClient();
+                                        Request request = new Request.Builder().url("https://maps.googleapis.com/maps/api/distancematrix/json?origins="+source_latitudes.get(0)+","+source_longitudes.get(0)+"&destinations="+destination_latitudes.get(0)+","+destination_longitudes.get(0)+"&key=AIzaSyB_A_3lfpzS0oSp0p9Q2XTqOtBe9TmbDuo").build();
+                                        Response response = null;
+                                        try {
+                                            response = client.newCall(request).execute();
+                                            Log.d("before_result:","Before Result");
+                                            Log.d("after_result:","After Result");
+                                            String responseToString = response.body().string();
+                                            Log.d("after_response:","After Response to String");
+                                            Log.d("responseToString: ", ""+responseToString);
+                                            Log.d("after_response_log:","After Response to String log");
+                                            JSONObject Jobject = new JSONObject(responseToString);
+                                            JSONArray Jarray = Jobject.getJSONArray("rows");
+                                            JSONObject rowsObject = Jarray.getJSONObject(0);
+                                            JSONArray elementsArray = rowsObject.getJSONArray("elements");
+                                            JSONObject elementsObject = elementsArray.getJSONObject(0);
+                                            JSONObject distanceObject = elementsObject.getJSONObject("distance");
+                                            Log.d("distance_object",""+distanceObject.toString());
+                                            JSONObject durationObject = elementsObject.getJSONObject("duration");
+                                            Log.d("duration_object",""+durationObject.toString());
+                                            String distance = (String) distanceObject.get("text");
+                                            String duration = (String) durationObject.get("text");
+                                            Log.d("distance: ",""+distance);
+                                            Log.d("duration: ",""+duration);
+                                            distance_array.add(distance);
+                                            duration_array.add(duration);
+                                        } catch (IOException | JSONException ioException) {
+                                            ioException.printStackTrace();
+                                        }
+                                        marker.setTitle("name:"+data.get(0)+" ,distance:"+distance_array.get(0)+" ,duration:"+duration_array.get(0));
+                                        showMessage(marker);
+                                        return true;
                                     }
                                 });
                             }
@@ -292,6 +374,11 @@ public class ReceiverFragment extends Fragment implements OnMapReadyCallback {
         Canvas canvas = new Canvas(bitmap);
         vectorDrawable.draw(canvas);
         return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
+
+    public void enableStrictMode(){
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
     }
 
 
